e8b3d15c2364c36cc3d387428d0c473d173dd67a
Jon Massey <jm4359@bristol.ac.uk>
2020-01-28 20:53:07 +0000

    db context and design-time factory. TODO: change IEnumerable<String> typed properties in model classes to defined types

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VetMedData.NET.DAL
-{
-    public class Class1
-    {
-    }
-}
---
+++ VetMedDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VetMedData.NET.Model;
+
+namespace VetMedData.NET.DAL
+{
+    public class VetMedDbContext :DbContext
+    {
+        private readonly string _connectionString;
+        public DbSet<Package> Packages { get; set; }
+        public DbSet<CurrentlyAuthorisedProduct> Products { get; set; }
+        public DbSet<SuspendedProduct> SuspendedProducts { get; set; }
+        public DbSet<ExpiredProduct> ExpiredProducts { get; set; }
+        public DbSet<HomoeopathicProduct> HomoeopathicProducts { get; set; }
+        public  DbSet<PackagedProduct> PackagedProducts{ get; set; }
+        public DbSet<TargetSpecies> TargetSpecies { get; set; }
+        public DbSet<PackageType> PackageTypes { get; set; }
+
+        public VetMedDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+    }
+}
